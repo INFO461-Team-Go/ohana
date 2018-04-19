efbabd1dac7fe35f4972c6cb2d5f58999ad1403a
John Lim <wjlim@uw.edu>
2018-04-19 10:07:18 -0700

    hooked up signup

--- web-app/src/Components/Constants.js
+++ web-app/src/Components/Constants.js
@@ -1,6 +1,7 @@
-export default {
+export default  {
     routes: {
         signin: "/",
+        signup: "/signup",
         views: "/view/:tabName",
         roommates: "/view/roomates",
         tasks: "/view/tasks"
--- web-app/src/Components/SignUp.js
+++ web-app/src/Components/SignUp.js
@@ -2,11 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class SignUp extends React.Component {
+
+    componentDidMount() {
+        console.log("signup view did mount");
+    }
+
     render() {
         return (
             <div className="container">
                 <p>This is the sign up view</p>
-                <button className="btn btn-primary"><Link>Sign in</Link></button>
+                <button className="btn btn-primary">Sign Up</button>
             </div>
         )
     }
