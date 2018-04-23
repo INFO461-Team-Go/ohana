b8e7feeec411d83d798b22e6fd1e2ef613ec06f5
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-04-23 15:41:36 -0700

    Added Firebase setup code

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+var config = {
+    apiKey: "AIzaSyCh7wRFGqzNXGuKBLzPYItxrhz8S-9b2aY",
+    authDomain: "ohana-e7233.firebaseapp.com",
+    databaseURL: "https://ohana-e7233.firebaseio.com",
+    projectId: "ohana-e7233",
+    storageBucket: "ohana-e7233.appspot.com",
+    messagingSenderId: "915757022426"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
