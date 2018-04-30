30517f235cc7af0bec06d3072b12c30db69cc283
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-04-30 16:01:29 -0700

    Implemented code to launch Ohana on Alexa

--- ohana-skill/index.js
+++ ohana-skill/index.js
@@ -1,1 +1,30 @@
-"use strict";
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+
+        const speakOutput = "Hello! Welcome to ohana!";
+
+        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .withSimpleCard('Ohana!', speakOutput)
+            .getResponse();
+    },
+};
+
+/* LAMBDA SETUP */
+const skillBuilder = Alexa.SkillBuilders.custom();
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchRequestHandler
+    )
+    .lambda();
