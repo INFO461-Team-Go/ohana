49a22a27dbff68d69fda92aafbca2507e363c322
John Lim <wjlim@uw.edu>
2018-05-12 15:44:40 -0700

    signin view complete

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Switch, Redirect, Route } from 'react-router-dom'
 import './global.css'
 import './content.css';
 import './home.css';
+import './log.css';
 
 
 import constants from "./Components/Constants";
