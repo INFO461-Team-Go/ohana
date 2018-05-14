f71f16a8d92902f48e21f43e1f9cf92a7f725534
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-05-14 16:42:38 -0700

    removed buggy files

--- ohana-skill/index.js
+++ ohana-skill/index.js
@@ -1,12 +1,5 @@
-/**
- * Harshitha Akkaraju
- * 
- * 
- */
-
 const Alexa = require('ask-sdk-core');
-const request = require('requests');
-const FirebaseDB = require('./dbConn.js');
+const Request = require('request');
 
 const amznProfileURL = 'https://api.amazon.com/user/profile?access_token=';
 
@@ -16,26 +9,27 @@ const LaunchRequestHandler = {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
+
         const user = handlerInput.requestEnvelope.session.user;
         if (user.userId == "amzn1.ask.account.[unique-value-here]" || user.userId == undefined) {
-            let output = "to start using this skill, please use the companion app to authenticate on Amazon"
-            handlerInput.responseBuilder
+            let output = "to start using this skill, please use the companion app to authenticate on Amazon";
+            return handlerInput.responseBuilder
                 .speak(output)
-                .withLinkAccountCard();
-            return;
+                .withLinkAccountCard()
+                .getResponse();
         }
-        request(amznProfileURL + user.accessToken, function (error, response, body) {
+        Request(amznProfileURL + user.accessToken, function (error, response, body) {
             if (response.statusCode == 200) {
                 let profile = JSON.parse(body);
                 console.log(profile);
                 // TODO: Extract email address and push it to Firebase
             }
         });
-        const speakOutput = "Hello! Welcome to ohana!";
+        const speakOutput = 'Hello! Welcome to ohana!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .getResponse();
-    },
+    }
 };
 
 // Lambda Setup
