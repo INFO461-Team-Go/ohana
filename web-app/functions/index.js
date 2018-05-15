b2639450c2fa92ab7e2e5add2a5a1a9a217a8d0b
John Lim <wjlim@uw.edu>
2018-05-15 16:15:10 -0700

    header bar

--- web-app/functions/index.js
+++ web-app/functions/index.js
@@ -13,3 +13,4 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
+
