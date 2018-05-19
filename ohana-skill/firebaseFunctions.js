88244cfa122ac0cdc24e744d9dae1c6fb3153f36
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-05-18 17:15:17 -0700

    Updated index.js + zip folder

---
+++ ohana-skill/firebaseFunctions.js
@@ -0,0 +1,20 @@
+const firebase = require("firebase");
+const md5 = require('md5');
+
+const config = {
+    apiKey: "AIzaSyCh7wRFGqzNXGuKBLzPYItxrhz8S-9b2aY",
+    authDomain: "ohana-e7233.firebaseapp.com",
+    databaseURL: "https://ohana-e7233.firebaseio.com",
+    storageBucket: "ohana-e7233.appspot.com",
+};
+firebase.initializeApp(config);
+const db = firebase.database();
+const accounts = db.ref(`/accounts`);
+
+exports.getTask = function (name) {
+    
+}
+
+exports.markAsDone = function (name) {
+    
+}
--- ohana-skill/index.js
+++ ohana-skill/index.js
@@ -1,15 +1,19 @@
+// TODO: Delete this comment: npm i -S grpc --target=8.1.0 --target_arch=x64 --target_platform=linux --target_libc=glibc
+'use strict';
+
 const Alexa = require('ask-sdk-core');
 const Request = require('request');
+const Firebase = require('./firebaseFunctions');
 
+// Amazon Profile URL
 const amznProfileURL = 'https://api.amazon.com/user/profile?access_token=';
 
-// LaunchRequestHandler: Welcome message when user invokes the skill
+// LaunchRequestHandler:
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-
         const user = handlerInput.requestEnvelope.session.user;
         if (user.userId == "amzn1.ask.account.[unique-value-here]" || user.userId == undefined) {
             let output = "to start using this skill, please use the companion app to authenticate on Amazon";
@@ -21,8 +25,6 @@ const LaunchRequestHandler = {
         Request(amznProfileURL + user.accessToken, function (error, response, body) {
             if (response.statusCode == 200) {
                 let profile = JSON.parse(body);
-                console.log(profile);
-                // TODO: Extract email address and push it to Firebase
             }
         });
         const speakOutput = 'Hello! Welcome to ohana!';
@@ -32,10 +34,66 @@ const LaunchRequestHandler = {
     }
 };
 
+const GetTaskIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest' && 
+            handlerInput.requestEnvelope.request.type === "GetTaskIntent";
+    },
+    handle(handlerInput) {
+        // do something
+    }
+};
+
+const MarkAsDoneIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest' && 
+            handlerInput.requestEnvelope.request.type === "MarkAsDoneIntent";
+    },
+    handle(handlerInput) {
+
+    }
+}
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+            handlerInput.requestEnvelope.request.type === "AMAZON.HelpIntent";
+    },
+    handle(handlerInput) {
+
+    }
+}
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+            (handlerInput.requestEnvelope.request.type === "AMAZON.CancelIntent" ||
+            handlerInput.requestEnvelope.request.type === "AMAZON.StopIntent");
+    },
+    handle(handlerInput) {
+
+    }
+}
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
+    },
+    handle(handlerInput) {
+        // clean up logic (if any)
+        return handlerInput.responseBuilder.getResponse();
+    }
+}
+
 // Lambda Setup
 const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
     .addRequestHandlers(
-        LaunchRequestHandler
+        LaunchRequestHandler,
+        GetTaskIntentHandler,
+        MarkAsDoneIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler
     )
     .lambda();
