0a71a4cf3d823ba7b7f8a225d8ac7d0e89fa5862
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-05-21 23:10:59 -0700

    Identified the time-out bug; it's in the firebase code

--- ohana-skill/index.js
+++ ohana-skill/index.js
@@ -1,12 +1,11 @@
-// TODO: Delete this comment: npm i -S grpc --target=8.1.0 --target_arch=x64 --target_platform=linux --target_libc=glibc
 'use strict';
-
 const Alexa = require('ask-sdk-core');
 const Request = require('request');
-const Firebase = require('./firebaseFunctions');
+const DBFunctions = require('./firebase-functions');
 
 // Amazon Profile URL
 const amznProfileURL = 'https://api.amazon.com/user/profile?access_token=';
+// const firebase = new DBFunctions();
 
 // LaunchRequestHandler:
 const LaunchRequestHandler = {
@@ -14,6 +13,7 @@ const LaunchRequestHandler = {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
+        handlerInput.requestEnvelope.context.callBackWaitsForEmptyEventLoop = false; 
         const user = handlerInput.requestEnvelope.session.user;
         if (user.userId == "amzn1.ask.account.[unique-value-here]" || user.userId == undefined) {
             let output = "to start using this skill, please use the companion app to authenticate on Amazon";
@@ -25,6 +25,9 @@ const LaunchRequestHandler = {
         Request(amznProfileURL + user.accessToken, function (error, response, body) {
             if (response.statusCode == 200) {
                 let profile = JSON.parse(body);
+                console.log(profile.email);
+                // TODO: Delete this later
+                // firebase.getTask("harshi");
             }
         });
         const speakOutput = 'Hello! Welcome to ohana!';
@@ -37,53 +40,71 @@ const LaunchRequestHandler = {
 const GetTaskIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest' && 
-            handlerInput.requestEnvelope.request.type === "GetTaskIntent";
+            handlerInput.requestEnvelope.request.type === 'GetTaskIntent';
     },
     handle(handlerInput) {
         // do something
+        const speakOutput = 'Hello! Welcome to ohana!';
+        console.log("inside GetTaskIntentHandler");
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
     }
 };
 
 const MarkAsDoneIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest' && 
-            handlerInput.requestEnvelope.request.type === "MarkAsDoneIntent";
+            handlerInput.requestEnvelope.request.type === 'MarkAsDoneIntent';
     },
     handle(handlerInput) {
-
+        const speakOutput = 'Hello! Welcome to ohana!';
+        console.log("inside MarkAsDoneIntentHandler");
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
     }
-}
+};
 
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-            handlerInput.requestEnvelope.request.type === "AMAZON.HelpIntent";
+            handlerInput.requestEnvelope.request.type === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-
+        const speakOutput = 'Hello! Welcome to ohana!';
+        console.log("inside HelpIntentHandler");
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
     }
-}
+};
 
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-            (handlerInput.requestEnvelope.request.type === "AMAZON.CancelIntent" ||
-            handlerInput.requestEnvelope.request.type === "AMAZON.StopIntent");
+            (handlerInput.requestEnvelope.request.type === 'AMAZON.CancelIntent' ||
+            handlerInput.requestEnvelope.request.type === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-
+        const speakOutput = 'Hello! Welcome to ohana!';
+        console.log("inside CancelIntentHandler");
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
     }
-}
+};
 
 const SessionEndedRequestHandler = {
     canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
     },
     handle(handlerInput) {
         // clean up logic (if any)
+        console.log("inside SessionEnded");
         return handlerInput.responseBuilder.getResponse();
     }
-}
+};
 
 // Lambda Setup
 const skillBuilder = Alexa.SkillBuilders.custom();
@@ -93,7 +114,6 @@ exports.handler = skillBuilder
         GetTaskIntentHandler,
         MarkAsDoneIntentHandler,
         HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        SessionEndedRequestHandler
+        CancelAndStopIntentHandler
     )
     .lambda();
