1e935a4be0cb6dd91cefb7b351b49ab87b9e309b
antonzheng <anton.zheng@gmail.com>
2018-05-26 15:01:51 -0700

    styling bullshit

--- web-app/src/Components/TaskCard.jsx
+++ web-app/src/Components/TaskCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-export default class NameCard extends React.Component {
+export default class TaskCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
