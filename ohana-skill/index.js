ef17834a2c9f2d1db5ab1df8fab2388d6d827263
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-05-27 18:12:53 -0700

    Functional code to make profile request

--- ohana-skill/index.js
+++ ohana-skill/index.js
@@ -1,8 +1,9 @@
 'use strict';
 // Dependencies
 const Alexa = require('ask-sdk-core');
-const request = require('request');
+const Https = require('https');
 const Firebase = require("firebase");
+require('firebase/database');
 const MD5 = require('md5');
 
 // Used to initializa Firebase app
@@ -12,9 +13,10 @@ const config = {
     databaseURL: "https://ohana-e7233.firebaseio.com",
     storageBucket: "ohana-e7233.appspot.com",
 };
+Firebase.initializeApp(config);
 
 // Constants
-//const db = Firebase.database();
+const database = Firebase.database();
 const amznProfileURL = 'https://api.amazon.com/user/profile?access_token=';
 
 /// REQUEST HANDLERS: Code to handle user interaction with Ohana
@@ -35,16 +37,6 @@ const LaunchRequestHandler = {
                 .withLinkAccountCard()
                 .getResponse();
         }
-        /*
-        request(amznProfileURL + user.accessToken, function (error, response, body) {
-            console.log("here");
-            if (response.statusCode == 200) {
-                let profile = JSON.parse(body);
-                console.log(profile.email);
-                
-            }
-        });
-        */
         const speakOutput = 'Hello! Welcome to ohana!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -60,47 +52,29 @@ const GetTaskIntentHandler = {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest' && 
             handlerInput.requestEnvelope.request.intent.name === 'GetTaskIntent';
     },
-    handle(handlerInput) {
+    async handle(handlerInput) {
         const user = handlerInput.requestEnvelope.session.user;
         const attributes = handlerInput.attributesManager.getSessionAttributes();
-        const response = handlerInput.responseBuilder;
-        const url = amznProfileURL + user.accessToken;
-        console.log(url);
-        let promise = getProfile(url);
-        promise.then(function(result) {
-            console.log(result);
-        }, function(error) {
-            console.log(error);
-        });
-        /*
-        let speakOutput = "Will do! What's your name?";
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse();
-        */
-    }
-};
-
-function getProfile(url) {
-    let options = {
-        url: url,
-        headers: {
-            'User-Agent': 'request'
+        let outputSpeech = '';
+        try {
+            const response = await httpsGet(user.accessToken);
+            console.log(response);
+            attributes.email = response.email;
+            attributes.hash = MD5(response.email);
+            handlerInput.attributesManager.setSessionAttributes(attributes);
+            outputSpeech = "Will do! What is your name?";
+            return handlerInput.responseBuilder
+                .speak(outputSpeech)
+                .reprompt(outputSpeech)
+                .getResponse();
+        } catch (error) {
+            outputSpeech = 'I am really sorry. I am unable to access part of my memory. Please try again later';
+            return handlerInput.responseBuilder
+                .speak(outputSpeech)
+                .getResponse();
         }
     }
-    console.log("Inside getProfile()");
-    return new Promise(function(resolve, reject) {
-        console.log("Inside promise");
-        request(options, function(error, response, body) {
-            console.log("Received a response");
-            if (error) {
-                reject(error);
-            } else {
-                resolve(JSON.parse(body));
-            }
-        });
-    });
-}
+};
 
 const MarkAsDoneIntentHandler = {
     canHandle(handlerInput) {
@@ -156,8 +130,7 @@ const CancelAndStopIntentHandler = {
             handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speakOutput = 'Hello! Welcome to ohana!';
-        console.log("inside CancelIntentHandler");
+        const speakOutput = 'Bye';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .getResponse();
@@ -170,14 +143,58 @@ const SessionEndedRequestHandler = {
     },
     handle(handlerInput) {
         // clean up logic (if any)
-        console.log("inside SessionEnded");
         return handlerInput.responseBuilder.getResponse();
     }
 };
 
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${handlerInput.requestEnvelope.request.type} ${handlerInput.requestEnvelope.request.type === 'IntentRequest' ? `intent: ${handlerInput.requestEnvelope.request.intent.name} ` : ''}${error.message}.`);
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+    },
+};
+
+function httpsGet(accessToken) {
+    let options = {
+        host: 'api.amazon.com',
+        port: 443,
+        path: '/user/profile?access_token=' + encodeURIComponent(accessToken),
+        method: 'GET'
+    };
+    return new Promise(((resolve, reject) => {
+        const request = Https.request(options, (response) => {
+            response.setEncoding('utf8');
+            let returnData = '';
+
+            if (response.statusCode < 200 || response.statusCode >= 300) {
+                return reject(new Error(`${response.statusCode}: ${response.req.getHeader('host')} ${response.req.path}`));
+            }
+
+            response.on('data', (chunk) => {
+                returnData += chunk;
+            });
+
+            response.on('end', () => {
+                resolve(JSON.parse(returnData));
+            });
+
+            response.on('error', (error) => {
+                reject(error);
+            });
+        });
+        request.end();
+    }));
+}
+
 // Lambda Setup
 let skill = undefined;
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
     context.callbackWaitsForEmptyEventLoop = false;
     // This snippet is needed for Firebase initialization on lambda
     if (Firebase.apps.length === 0) {
@@ -196,7 +213,23 @@ exports.handler = async function(event, context) {
                 CancelAndStopIntentHandler,
                 SessionEndedRequestHandler
             )
+            .addErrorHandlers(ErrorHandler)
             .create();
     }
     return skill.invoke(event, context);
 }
+
+/*
+const skillBuilder = Alexa.SkillBuilders.custom();
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        GetTaskIntentHandler,
+        MarkAsDoneIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
+*/
